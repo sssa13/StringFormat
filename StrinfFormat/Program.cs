64d5c86ea013bab48f12c49c3e581831a6f0f075
sssa13 <sssa13@yandex.ru>
2017-05-15 22:33:44 +0300

    work

---
+++ StrinfFormat/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrinfFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = new List<string>();
+            numbers.Add("23.8976,12.3218");
+            numbers.Add("25.7639,11.9463");
+            numbers.Add("24.8293,12.2134");
+
+           
+
+             //    foreach (string str1 in numbers)
+             //    {
+             //        Console.Write("X:" + "{0:00,0000}", str1.Remove(7));
+             //        Console.WriteLine(" Y:" + "{0:00.0000}", str1.Substring(8));
+             //    }
+
+
+
+            int i = 0;                                              
+            while (i < numbers.Count)
+            {
+                string str1 = numbers[i];
+                Console.Write("X:" + "{0:00,0000}", str1.Remove(7));  // удаляем после 7-го символа
+                Console.WriteLine(" Y:" + "{0:00.0000}", str1.Substring(8));   // // удаляем до 8-го символа
+                i++;
+            }
+
+
+
+          
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ StrinfFormat/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("StrinfFormat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StrinfFormat")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ec63a90d-5ace-49c6-870a-1261f8f7740a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
